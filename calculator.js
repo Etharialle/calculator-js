2509e6e787ffef566ba4918a36628314186b5fdf
Christopher Adams <christopher.m.adams2@gmail.com>
2024-05-04 16:09:36 -0700

    added eventlisteners for 0 and 1 buttons

--- calculator.js
+++ calculator.js
@@ -1,14 +1,44 @@
 // Variable declarations
+let numberOne = "";
+let numberTwo = "";
+let operator = "";
+
 // Event Listeners
-// 
+// Declarations
 const zeroDigit = document.querySelector("#zero-key");
 const oneDigit = document.querySelector("#one-key");
+const twoDigit = document.querySelector("#two-key");
+const threeDigit = document.querySelector("#three-key");
+const fourDigit = document.querySelector("#four-key");
+const fiveDigit = document.querySelector("#five-key");
+const sixDigit = document.querySelector("#six-key");
+const sevenDigit = document.querySelector("#seven-key");
+const eightDigit = document.querySelector("#eight-key");
+const nineDigit = document.querySelector("#nine-key");
+const decimalKey = document.querySelector("#decimal-key");
+const plusMinusKey = document.querySelector("#plus-minus-key");
 
+// TODO: prevent leading 0 unless second character is a "."
+zeroDigit.addEventListener("click", function (e) {
+    if (operator == "" && numberOne !== "0") {
+        numberOne = String(numberOne) + "0";
+        console.log(numberOne);
+    }
+});
+oneDigit.addEventListener("click", function (e) {
+    if (operator == "") {
+        numberOne = String(numberOne) + "1";
+        console.log(numberOne);
+    }
+});
+
+
+console.log(add(5,3));
 // Math operations
 function add(numberOne, numberTwo) {
     return numberOne + numberTwo;
 }
-console.log(add(5,3));
+
 function subtract(numberOne, numberTwo) {
     return numberOne - numberTwo;
 }
@@ -44,4 +74,5 @@ function operate (numberOne, numberTwo, operator) {
 
 
 // Exports for testing
-export = {add, subtract, multiply, divide};
+//export const {add, subtract, multiply, divide};
+//const module.exports = add(numberOne, numberTwo;
